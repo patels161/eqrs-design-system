ced38e30bc1d228e6161b2ce37b6c57374cbbebf
Sajani Patel <spatel@nwed.esrd.net>
2018-03-09 13:32:11 -0500

    Resoloved merge conflicts

--- eqrsdesignsystem/src/app/app.module.ts
+++ eqrsdesignsystem/src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatTabsModule} from '@angular/material/tabs';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-
-
 import { AppComponent } from './app.component';
 import { ComponentNameComponent } from './side-nav/component-name.component';
 import { MainContentComponent } from './main-content/main-content.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 
 @NgModule({
