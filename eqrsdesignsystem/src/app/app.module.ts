5e546b73792e7bc22b7376ed28514511cdd99f23
am3436 <spatel@nwed.esrd.net>
2018-03-21 12:57:40 -0400

    Added routing for getting-started section.

--- eqrsdesignsystem/src/app/app.module.ts
+++ eqrsdesignsystem/src/app/app.module.ts
@@ -5,24 +5,12 @@ import { SideNavComponent } from './side-nav/side-nav.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import {MatTabsModule} from '@angular/material/tabs';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {RouterModule, Routes} from "@angular/router";
 import { GettingStartedComponent } from './main-content/getting-started/getting-started.component';
 import { StyleComponent } from './main-content/style/style.component';
 import { ComponentsComponent } from './main-content/components/components.component';
-import { ColorsComponent } from './main-content/style/colors/colors.component';
-import { GridComponent } from './main-content/style/grid/grid.component';
-import { IconographyComponent } from './main-content/style/iconography/iconography.component';
-import { TypographyComponent } from './main-content/style/typography/typography.component';
 import {StyleModule} from "./main-content/style/style.module";
 import {GettingStartedModule} from "./main-content/getting-started/getting-started.module";
-
-const appRoutes: Routes = [
-  { path: '', redirectTo: '',  pathMatch: 'full'},
-  { path: 'grid', component: GridComponent},
-  { path: 'colors', component: ColorsComponent},
-  { path: 'iconography', component: IconographyComponent},
-  { path: 'typography', component: TypographyComponent}
-];
+import {routing} from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -39,7 +27,7 @@ const appRoutes: Routes = [
     BrowserAnimationsModule,
     StyleModule,
     GettingStartedModule,
-    RouterModule.forRoot(appRoutes)
+    routing
 ],
   providers: [],
   bootstrap: [AppComponent]
