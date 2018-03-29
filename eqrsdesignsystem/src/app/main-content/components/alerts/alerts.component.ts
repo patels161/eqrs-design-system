efdb62bfd320c16425d203bb23ea44c882999b84
Sajani Patel <spatel@nwed.esrd.net>
2018-03-29 11:41:36 -0400

    ngmodels for all alert messages code blocks.

--- eqrsdesignsystem/src/app/main-content/components/alerts/alerts.component.ts
+++ eqrsdesignsystem/src/app/main-content/components/alerts/alerts.component.ts
@@ -6,16 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alerts.component.css']
 })
 export class AlertsComponent implements OnInit {
-  visibility: boolean = false;
+  successShow: boolean = false;
+  warningShow: boolean = false;
+  errorShow: boolean = false;
+  errorMultilineShow: boolean = false;
+  errorSinglelineShow: boolean = false;
+  informationShow: boolean = false;
   show: boolean = true;
   htmlLanguage = 'html';
   cssLanguage = 'css';
-  cssContent = '<div class="eqrs-alert-success  col-md-12">\n' +
+  successStatusContent = '<div class="eqrs-alert-success  col-md-12">\n' +
     '               <div class="eqrs-alert-body">\n' +
     '                 <h4 class="eqrs-alert-heading">Success status</h4>\n' +
     '                 <p>Displays when database change is successful.</p>\n' +
     '               </div>\n' +
     '             </div>';
+  warningStatusContent = '';
+  errorStatusContent = '';
+  errorMultilineContent = '';
+  errorSinglelineContent = '';
+  informationstStatusContent = '';
 
   constructor() { }
 
