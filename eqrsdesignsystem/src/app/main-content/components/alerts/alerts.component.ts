2272b61fc439273e5ffb4b309a8c770e1c063ca5
Sajani Patel <spatel@nwed.esrd.net>
2018-03-29 14:03:18 -0400

    Added routing for previous and next link in alerts

--- eqrsdesignsystem/src/app/main-content/components/alerts/alerts.component.ts
+++ eqrsdesignsystem/src/app/main-content/components/alerts/alerts.component.ts
@@ -12,57 +12,64 @@ export class AlertsComponent implements OnInit {
   errorMultilineShow: boolean = false;
   errorSinglelineShow: boolean = false;
   informationShow: boolean = false;
-  show: boolean = true;
   htmlLanguage = 'html';
   cssLanguage = 'css';
-  successStatusContent = '<div class="eqrs-alert-success  col-md-12">\n' +
-    '               <div class="eqrs-alert-body">\n' +
-    '                 <h4 class="eqrs-alert-heading">Success status</h4>\n' +
-    '                 <p>Displays when database change is successful.</p>\n' +
-    '               </div>\n' +
-    '             </div>';
-  warningStatusContent = '<div class="eqrs-alert-warning  col-md-12">\n' +
-    '               <div class="eqrs-alert-body">\n' +
-    '                 <h4 class="eqrs-alert-heading">Warning status</h4>\n' +
-    '                 <p>Displays when database change results in error but we allow user to temporarily persist the bad data. User action will eventually be needed to correct the bad data.</p>\n' +
-    '               </div>\n' +
-    '             </div>';
-  errorStatusContent = '<div class="eqrs-alert-error  col-md-12">\n' +
-    '               <div class="eqrs-alert-body">\n' +
-    '                 <h4 class="eqrs-alert-heading">Error status</h4>\n' +
-    '                 <p>Displays when database change fails. User action needed in order to save.</p>\n' +
-    '               </div>\n' +
-    '             </div>';
-  errorMultilineContent = '<div class="eqrs-alert-error  col-md-12">\n' +
-    '               <div class="eqrs-alert-body">\n' +
-    '                 <h4 class="eqrs-alert-heading">Error single line</h4>\n' +
-    '                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>\n' +
-    '                 <ul class="eqrs-single-list-item">\n' +
-    '                 <li>This is item A</li>\n' +
-    '                 <li>This is item B</li>\n' +
-    '                 <li>This is item C</li>\n' +
-    '                 </ul>\n' +
-    '               </div>\n' +
-    '             </div>';
-  errorSinglelineContent = '<div class="eqrs-alert-error  col-md-12">\n' +
-    '               <div class="eqrs-alert-body">\n' +
-    '                 <h4 class="eqrs-alert-heading">Error single line</h4>\n' +
-    '                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>\n' +
-    '                 <ul class="eqrs-single-list-item">\n' +
-    '                 <li>This is item A</li>\n' +
-    '                 </ul>\n' +
-    '               </div>\n' +
-    '             </div>';
-  informationstStatusContent = '<div class="eqrs-alert-error  col-md-12">\n' +
-    '               <div class="eqrs-alert-body">\n' +
-    '                 <h4 class="eqrs-alert-heading">Information status</h4>\n' +
-    '                <p>Displays when feedback should be provided to the user. No user action needed.</p>\n' +
-    '               </div>\n' +
-    '             </div>';
+  successStatusContent: any;
+  warningStatusContent: any;
+  errorStatusContent: any;
+  errorMultilineContent: any;
+  errorSinglelineContent: any;
+  informationStatusContent: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.successStatusContent = '<div class="eqrs-alert-success  col-md-12">\n' +
+      '               <div class="eqrs-alert-body">\n' +
+      '                 <h4 class="eqrs-alert-heading">Success status</h4>\n' +
+      '                 <p>Displays when database change is successful.</p>\n' +
+      '               </div>\n' +
+      '             </div>';
+    this.warningStatusContent = '<div class="eqrs-alert-warning  col-md-12">\n' +
+      '               <div class="eqrs-alert-body">\n' +
+      '                 <h4 class="eqrs-alert-heading">Warning status</h4>\n' +
+      '                 <p>Displays when database change results in error but we allow user to temporarily persist the bad data. User action will eventually be needed to correct the bad data.</p>\n' +
+      '               </div>\n' +
+      '             </div>';
+    this.errorStatusContent = '<div class="eqrs-alert-error  col-md-12">\n' +
+      '               <div class="eqrs-alert-body">\n' +
+      '                 <h4 class="eqrs-alert-heading">Error status</h4>\n' +
+      '                 <p>Displays when database change fails. User action needed in order to save.</p>\n' +
+      '               </div>\n' +
+      '             </div>';
+    this.errorMultilineContent = '<div class="eqrs-alert-error  col-md-12">\n' +
+      '               <div class="eqrs-alert-body">\n' +
+      '                 <h4 class="eqrs-alert-heading">Error single line</h4>\n' +
+      '                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>\n' +
+      '                 <ul class="eqrs-single-list-item">\n' +
+      '                 <li>This is item A</li>\n' +
+      '                 <li>This is item B</li>\n' +
+      '                 <li>This is item C</li>\n' +
+      '                 </ul>\n' +
+      '               </div>\n' +
+      '             </div>';
+    this.errorSinglelineContent = '<div class="eqrs-alert-error  col-md-12">\n' +
+      '               <div class="eqrs-alert-body">\n' +
+      '                 <h4 class="eqrs-alert-heading">Error single line</h4>\n' +
+      '                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>\n' +
+      '                 <ul class="eqrs-single-list-item">\n' +
+      '                 <li>This is item A</li>\n' +
+      '                 </ul>\n' +
+      '               </div>\n' +
+      '             </div>';
+    this.informationStatusContent = '<div class="eqrs-alert-error  col-md-12">\n' +
+      '               <div class="eqrs-alert-body">\n' +
+      '                 <h4 class="eqrs-alert-heading">Information status</h4>\n' +
+      '                <p>Displays when feedback should be provided to the user. No user action needed.</p>\n' +
+      '               </div>\n' +
+      '             </div>';
+  }
 }
+
+
