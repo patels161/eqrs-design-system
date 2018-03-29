8eb51c0d4534c861a8ca0f1fa415633a0aa82759
Lovemore C <lchirombo@nwed.esrd.net>
2018-03-29 13:34:50 -0400

    new code block updates for components

--- eqrsdesignsystem/src/app/main-content/components/alerts/alerts.component.ts
+++ eqrsdesignsystem/src/app/main-content/components/alerts/alerts.component.ts
@@ -21,11 +21,44 @@ export class AlertsComponent implements OnInit {
     '                 <p>Displays when database change is successful.</p>\n' +
     '               </div>\n' +
     '             </div>';
-  warningStatusContent = '';
-  errorStatusContent = '';
-  errorMultilineContent = '';
-  errorSinglelineContent = '';
-  informationstStatusContent = '';
+  warningStatusContent = '<div class="eqrs-alert-warning  col-md-12">\n' +
+    '               <div class="eqrs-alert-body">\n' +
+    '                 <h4 class="eqrs-alert-heading">Warning status</h4>\n' +
+    '                 <p>Displays when database change results in error but we allow user to temporarily persist the bad data. User action will eventually be needed to correct the bad data.</p>\n' +
+    '               </div>\n' +
+    '             </div>';
+  errorStatusContent = '<div class="eqrs-alert-error  col-md-12">\n' +
+    '               <div class="eqrs-alert-body">\n' +
+    '                 <h4 class="eqrs-alert-heading">Error status</h4>\n' +
+    '                 <p>Displays when database change fails. User action needed in order to save.</p>\n' +
+    '               </div>\n' +
+    '             </div>';
+  errorMultilineContent = '<div class="eqrs-alert-error  col-md-12">\n' +
+    '               <div class="eqrs-alert-body">\n' +
+    '                 <h4 class="eqrs-alert-heading">Error single line</h4>\n' +
+    '                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>\n' +
+    '                 <ul class="eqrs-single-list-item">\n' +
+    '                 <li>This is item A</li>\n' +
+    '                 <li>This is item B</li>\n' +
+    '                 <li>This is item C</li>\n' +
+    '                 </ul>\n' +
+    '               </div>\n' +
+    '             </div>';
+  errorSinglelineContent = '<div class="eqrs-alert-error  col-md-12">\n' +
+    '               <div class="eqrs-alert-body">\n' +
+    '                 <h4 class="eqrs-alert-heading">Error single line</h4>\n' +
+    '                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>\n' +
+    '                 <ul class="eqrs-single-list-item">\n' +
+    '                 <li>This is item A</li>\n' +
+    '                 </ul>\n' +
+    '               </div>\n' +
+    '             </div>';
+  informationstStatusContent = '<div class="eqrs-alert-error  col-md-12">\n' +
+    '               <div class="eqrs-alert-body">\n' +
+    '                 <h4 class="eqrs-alert-heading">Information status</h4>\n' +
+    '                <p>Displays when feedback should be provided to the user. No user action needed.</p>\n' +
+    '               </div>\n' +
+    '             </div>';
 
   constructor() { }
 
