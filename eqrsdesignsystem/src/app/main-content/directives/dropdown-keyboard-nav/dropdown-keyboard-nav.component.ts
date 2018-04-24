cd02fbdf23147d58108e5f66d5cb66cc0900a30f
am3436 <spatel@nwed.esrd.net>
2018-04-24 16:37:11 -0400

    Added new components under directive menu

---
+++ eqrsdesignsystem/src/app/main-content/directives/dropdown-keyboard-nav/dropdown-keyboard-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown-keyboard-nav',
+  templateUrl: './dropdown-keyboard-nav.component.html',
+  styleUrls: ['./dropdown-keyboard-nav.component.css']
+})
+export class DropdownKeyboardNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
