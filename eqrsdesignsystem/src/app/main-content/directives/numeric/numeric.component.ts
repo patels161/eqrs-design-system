d7e2002d271a402ae2cb67138c084c6179587246
Sajani Patel <spatel@nwed.esrd.net>
2018-05-07 10:47:20 -0400

    Added date and numeric directives

--- eqrsdesignsystem/src/app/main-content/directives/numeric/numeric.component.ts
+++ eqrsdesignsystem/src/app/main-content/directives/numeric/numeric.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./numeric.component.css']
 })
 export class NumericComponent implements OnInit {
+  cssLanguage = 'css';
+  show: boolean = false;
+  content: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.content = '<input numeric name="xyz" >';
   }
 
 }
