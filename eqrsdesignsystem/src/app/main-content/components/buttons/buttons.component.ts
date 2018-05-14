1919783e0acddb98f3a1992644d1313711ef39b9
Sajani Patel <spatel@nwed.esrd.net>
2018-05-14 11:43:29 -0400

    small changes

--- eqrsdesignsystem/src/app/main-content/components/buttons/buttons.component.ts
+++ eqrsdesignsystem/src/app/main-content/components/buttons/buttons.component.ts
@@ -28,30 +28,30 @@ export class ButtonsComponent implements OnInit {
 
   ngOnInit() {
     this.primaryButtonContent = '<button class="eqrs-button-primary">Submit</button>\n' +
-      '               <button class="eqrs-button-primary primary-hover">Submit</button>\n' +
-      '                 <button class="eqrs-button-primary primary-active">Submit</button>';
+      '             <button class="eqrs-button-primary primary-hover">Submit</button>\n' +
+      '             <button class="eqrs-button-primary primary-active">Submit</button>';
 
     this.secondaryButtonContent = '<button class="eqrs-button-secondary">Cancel</button>\n' +
-      '               <button class="eqrs-button-secondary eqrs-button-secondary-hover">Cancel</button>\n' +
-      '                 <button class="eqrs-button-secondary eqrs-button-secondary-active">Cancel</button>' ;
+      '             <button class="eqrs-button-secondary eqrs-button-secondary-hover">Cancel</button>\n' +
+      '             <button class="eqrs-button-secondary eqrs-button-secondary-active">Cancel</button>' ;
 
     this.accentButtonContent = '<button class="eqrs-button-accent">Submit</button>\n' +
-      '               <button class="eqrs-button-accent accent-hover">Submit</button>\n' +
-      '                 <button class="eqrs-button-accent accent-active">Submit</button>';
+      '             <button class="eqrs-button-accent accent-hover">Submit</button>\n' +
+      '             <button class="eqrs-button-accent accent-active">Submit</button>';
 
     this.destructiveButtonContent = '<button class="eqrs-button-destructive">Delete</button>\n' +
-      '               <button class="eqrs-button-destructive eqrs-button-destructive-hover">Delete</button>\n' +
-      '                 <button class="eqrs-button-destructive eqrs-button-destructive-active">Delete</button>';
+      '             <button class="eqrs-button-destructive eqrs-button-destructive-hover">Delete</button>\n' +
+      '             <button class="eqrs-button-destructive eqrs-button-destructive-active">Delete</button>';
 
     this.disabledButtonContent = '<button class="eqrs-button-primary" disabled="">Disabled button</button>';
 
     this.exlayout1ButtonContent = '<button class="eqrs-button-destructive">Delete</button>\n' +
-      '               <button class="eqrs-button-secondary">Cancel</button>\n' +
-      '                <button class="eqrs-button-primary">Save</button>\n' +
-      '                 <button class="eqrs-button-accent" disabled="">Submit</button>';
+      '             <button class="eqrs-button-secondary">Cancel</button>\n' +
+      '             <button class="eqrs-button-primary">Save</button>\n' +
+      '             <button class="eqrs-button-accent" disabled="">Submit</button>';
 
     this.exlayout2ButtonContent = '<button class="eqrs-button-secondary focus">Cancel</button>\n' +
-      '                 <button class="eqrs-button-destructive">Delete</button>';
+      '             <button class="eqrs-button-destructive">Delete</button>';
 
     this.buttonlinksButtonContent = '<button class="eqrs-edit-link">This is a button not a link</button>';
   }
