f960ff30ff79547f72fa3a8ccfb0c2c7bfda1a02
Sajani Patel <spatel@nwed.esrd.net>
2018-05-17 13:50:37 -0400

    Added labels

--- eqrsdesignsystem/src/app/main-content/components/labels/labels.component.ts
+++ eqrsdesignsystem/src/app/main-content/components/labels/labels.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-labels',
   templateUrl: './labels.component.html',
-  styleUrls: ['./labels.component.css']
+  styleUrls: ['./labels.component.scss']
 })
 export class LabelsComponent implements OnInit {
 
+  cssLanguage = 'css';
+  labelsContent: any;
   constructor() { }
 
   ngOnInit() {
+    this.labelsContent =
+      '<label class="eqrs-label-success">Edited</label>\n' +
+      '        <label class="eqrs-label-warning">Warning</label>';
   }
 
 }
